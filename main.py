26e028c4f2593db298a4c73253d905b1693dbc3e
Michael Eco <michael.gunawan2002@gmail.com>
2024-08-15 11:34:52 +0700

    Update main.py with streamlit integration

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from src.config.agents import Agent
-from langchain_core.messages import HumanMessage
+# from src.config.agents import Agent
+# from langchain_core.messages import HumanMessage
 
-# Config agent
-agent = Agent()
+# # Config agent
+# agent = Agent()
 
-# Add messages to memory
-messages = [HumanMessage(content="Hello, I'm Bob!")]
-response = agent.invoke(messages)
-print(response)
+# # Add messages to memory
+# messages = [HumanMessage(content="Hello, I'm Bob!")]
+# response = agent.invoke(messages)
+# print(response)
+
+import streamlit as st
+
+st.write("Hello, world!")
+
+with st.sidebar:
+    st.write("This is a sidebar")
