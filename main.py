120d7a0c0c10a9dbacc9cc86d4f0f98271305ee1
Michael Eco <michael.gunawan2002@gmail.com>
2024-08-15 12:51:12 +0700

    Add tool invocation functionality and prompt template scaffolding

--- main.py
+++ main.py
@@ -34,8 +34,84 @@ selected_end_time = datetime.datetime.strptime("12:00", "%H:%M").time()
 #                 and selected_start_time <= end_time <= selected_end_time:
 #             print(name, date, start_time, end_time)
 
-from src.tools.rag import DocumentLoader
+from src.config.agents import Agent
+from langchain_core.tools import tool
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import render_text_description
 
-loader = DocumentLoader("data/booking.csv")
-for record in loader.lazy_load():
-    print(record.metadata)
+agent = Agent()
+
+@tool
+def add(x: float, y: float) -> float:
+    """
+    This function takes two float numbers as input and returns their sum.
+
+    Parameters:
+        x (float): The first number.
+        y (float): The second number.
+
+    Returns:
+        float: The sum of x and y.
+    """
+    return x + y
+
+tools = [add]
+rendered_tools = render_text_description(tools)
+system_prompt = f"""\
+You are an assistant that has access to the following set of tools. 
+Here are the names and descriptions for each tool:
+
+{rendered_tools}
+
+Given the user input, return the name and input of the tool to use. 
+Return your response as a JSON blob with 'name' and 'arguments' keys.
+
+The `arguments` should be a dictionary, with keys corresponding 
+to the argument names and the values corresponding to the requested values.
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system_prompt), ("user", "{input}")]
+)
+
+chain = prompt | agent
+message = chain.invoke({"input": "what's 3 plus 1132"})
+
+chain = prompt | agent | JsonOutputParser()
+
+from typing import Any, Dict, Optional, TypedDict
+
+from langchain_core.runnables import RunnableConfig
+
+
+class ToolCallRequest(TypedDict):
+    """A typed dict that shows the inputs into the invoke_tool function."""
+
+    name: str
+    arguments: Dict[str, Any]
+
+
+def invoke_tool(
+    tool_call_request: ToolCallRequest, config: Optional[RunnableConfig] = None
+):
+    """A function that we can use the perform a tool invocation.
+
+    Args:
+        tool_call_request: a dict that contains the keys name and arguments.
+            The name must match the name of a tool that exists.
+            The arguments are the arguments to that tool.
+        config: This is configuration information that LangChain uses that contains
+            things like callbacks, metadata, etc.See LCEL documentation about RunnableConfig.
+
+    Returns:
+        output from the requested tool
+    """
+    tool_name_to_tool = {tool.name: tool for tool in tools}
+    name = tool_call_request["name"]
+    requested_tool = tool_name_to_tool[name]
+    return requested_tool.invoke(tool_call_request["arguments"], config=config)
+
+chain = prompt | agent | JsonOutputParser() | invoke_tool
+print(chain.invoke({"input": "what's thirteen plus 4.14137281"}))
+
